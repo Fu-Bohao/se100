43a2b6230c4ae486aced9d189cad6d9b7e7ac894
Fu-Bohao <fubohao29@gmail.com>
2024-11-11 20:14:09 +0800

    ex 1-4

--- L03/exercise1,2,3/scripts/script.js
+++ L03/exercise1,2,3/scripts/script.js
@@ -1,5 +1,19 @@
+//Ex1
+console.log("Hello World!");
+
+//Ex2
+const stockPrice = 300;
+console.log(stockPrice);
+
+//Ex3
 let money = 6000;
 money += 3000;
 console.log(money);
 money *= 3;
 console.log(money);
+
+//Ex4
+const companyName = "Amazon"
+const companyRating = 5
+
+console.log(`${companyName} has a customer satisfaction rating of ${companyRating}`)
